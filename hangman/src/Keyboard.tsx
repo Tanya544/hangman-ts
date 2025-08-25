a6de4d8123800b2b30e028e009706fd47b029220
Tanya <tanasenuk56@gmail.com>
2025-08-25 14:18:30 +0300

    All done

--- hangman/src/Keyboard.tsx
+++ hangman/src/Keyboard.tsx
@@ -30,42 +30,42 @@ const KEYS = [
 ]
 
 type KeyboardProps = {
-    disabled?: boolean
-    activeLetters: string[]
-    inactiveLetters: string[]
-    addGuessedLetter: (letter: string) => void
+  disabled?: boolean
+  activeLetters: string[]
+  inactiveLetters: string[]
+  addGuessedLetter: (letter: string) => void
 }
 
 export function Keyboard({
-    activeLetters,
-    inactiveLetters,
-    addGuessedLetter,
-    disabled = false,
+  activeLetters,
+  inactiveLetters,
+  addGuessedLetter,
+  disabled = false,
 }: KeyboardProps) {
-    return (
-        <div 
-        style={{
-            display: "grid",
-            gridTemplateColumns: "repeat(auto-fit,  minmax(75px, 1fr))",
-            gap: ".5rem",
-        }}
-        >
-            {KEYS.map(key => {
-                const isActive = activeLetters.includes(key)
-                const isInactive = inactiveLetters.includes(key)
-                return (
-                    <button 
-                    onClick={() => addGuessedLetter(key)}
-                    className={`${styles.btn} ${isActive ? styles.active : ""} ${
-                        isInactive ? styles.inactive : ""
-                        }`}
-                        disabled={isInactive || isActive || disabled}
-                        key={key}
-                        >
-                        {key}
-                    </button>
-                )
-            })}
-        </div>
-    )
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
+        gap: ".5rem",
+      }}
+    >
+      {KEYS.map(key => {
+        const isActive = activeLetters.includes(key)
+        const isInactive = inactiveLetters.includes(key)
+        return (
+          <button
+            onClick={() => addGuessedLetter(key)}
+            className={`${styles.btn} ${isActive ? styles.active : ""} ${
+              isInactive ? styles.inactive : ""
+            }`}
+            disabled={isInactive || isActive || disabled}
+            key={key}
+          >
+            {key}
+          </button>
+        )
+      })}
+    </div>
+  )
 }
